a5ea8194467824b131ecdba0b21d16cb4d92c501
jmk11 <z5160822@student.unsw.edu.au>
2020-03-15 00:08:17 +1100

    Remove junk comments

--- batteryalarm.sh
+++ batteryalarm.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 
-#sleeptime=60
-
 while true
 do
 	batterylevel=$(cat /sys/class/power_supply/BAT0/capacity)
 	if [[ $(upower -i $(upower -e | grep 'BAT') | grep -E "state") == "    state:               charging" ]]
 	then
-		#echo $batterylevel
 		if [[ $batterylevel -gt 84 ]]
 		then
-			#echo "b"
 			notify-send "Battery at "$batterylevel"%"
 			if gnome-screensaver-command -q | grep "is active" # the screen is locked
 			then
-				#echo "c"
 				while [[ $(upower -i $(upower -e | grep 'BAT') | grep -E "state") == "    state:               charging" ]]
 				do
-					#echo "d"
-					#printf "\a" 
-					#beep
 					spd-say -w "Battery charged. Unplug the computer."
-					sleep 1 #4
+					sleep 1
 				done
 				sleep 1
 				spd-say -w "Thank you."
@@ -40,7 +32,7 @@ do
 				while [[ $(upower -i $(upower -e | grep 'BAT') | grep -E "state") == "    state:               discharging" ]]
 	         	do
 		            spd-say -w "Battery low. Plug in the computer."
-	                sleep 1 #4
+	                sleep 1
 	            done
 	            sleep 1
 	            spd-say "Thank you."
@@ -50,24 +42,3 @@ do
 	sleep 60
 done
 
-: '
-while [[ ( $(upower -i $(upower -e | grep 'BAT') | grep -E "state") == "state:               charging" ) && ( $(cat /sys/class/power_supply/BAT0/capacity) < 80 ) ]]
-do
-	sleep 1
-	#cat /sys/class/power_supply/BAT0/capacity
-done
-beeps.sh
-'
-
-: <<'END_COMMENT'
-This is a heredoc (<<) redirected to a NOP command (:).
-The single quotes around END_COMMENT are important,
-because it disables variable resolving and command resolving
-within these lines.  Without the single-quotes around END_COMMENT,
-the following two $() `` commands would get executed:
-$(gibberish command)
-`rm -fr mydir`
-comment1
-comment2 
-comment3
-END_COMMENT
